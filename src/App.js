767daf02ce9a11902bb2fce396af4f698056c2a1
jeromevs <56254178+jeromevs@users.noreply.github.com>
2019-11-08 09:23:12 +0100

    final version

--- src/App.js
+++ src/App.js
@@ -3,13 +3,28 @@ import "./App.css";
 import Dollar from "./images/dollar.png";
 import Arrow from "./images/arrow.png";
 import Arrowup from "./images/arrowup.png";
+import rates from "./Rates";
+const deviseItems = [];
+Object.keys(rates).forEach(devise => {
+  deviseItems.push(devise);
+});
+console.log(deviseItems);
 
 function App() {
-  const [currency, setCurrency] = useState(0);
-  const [convertedCurrency, setConvertedCurrency] = useState(0);
+  const [currency1, setCurrency1] = useState("");
+  const [currency2, setCurrency2] = useState("");
+  const [button, setButton] = useState("");
+  const [devise1, setDevise1] = useState("USD");
+  const [devise2, setDevise2] = useState("EUR");
 
-  let euro = 1;
-  let dollar = 1.139283;
+  const handleSubmit = event => {
+    event.preventDefault();
+    if (button === "down") {
+      setCurrency2((currency1 * rates[devise2]) / rates[devise1]);
+    } else if (button === "up") {
+      setCurrency1((currency2 * rates[devise1]) / rates[devise2]);
+    }
+  };
 
   return (
     <>
@@ -18,65 +33,87 @@ function App() {
           <img
             src={Dollar}
             alt="dollar"
-            style={{ height: "30px", width: "100px" }}
+            style={{ height: "50px", width: "100px" }}
           />
           <h1>Eur to Usd</h1>
           <img
             src={Dollar}
             alt="dollar"
-            style={{ height: "30px", width: "100px" }}
+            style={{ height: "50px", width: "100px" }}
           />
         </div>
 
-        <form
-          onSubmit={event => {
-            event.preventDefault();
-
-            let newValue = (currency * dollar) / euro;
-            setConvertedCurrency(newValue);
-          }}
-        >
+        <form autoComplete="off" onSubmit={handleSubmit}>
           <p>
             <input
               type="text"
               name="toconvert"
               placeholder="0"
-              value={currency}
+              value={currency1}
               onChange={event => {
-                setCurrency(event.target.value);
+                setCurrency1(event.target.value);
               }}
             />
-            €
+            <select
+              value={devise1}
+              onChange={event => {
+                setDevise1(event.target.value);
+              }}
+            >
+              {deviseItems.map(deviseName => {
+                return <option value={deviseName}>{deviseName}</option>;
+              })}
+            </select>
           </p>
           <div className="arrow">
-            <button name="down" type="submit">
+            <button
+              onClick={() => {
+                setButton("down");
+              }}
+              name="down"
+              type="submit"
+            >
               <img
                 src={Arrow}
                 alt="arrow"
-                style={{ height: "50px", width: "50px" }}
+                style={{ height: "80px", width: "80px", border: "none" }}
               />
             </button>
 
-            <button name="up" type="submit">
+            <button
+              onClick={() => {
+                setButton("up");
+              }}
+              name="up"
+              type="submit"
+            >
               <img
                 src={Arrowup}
                 alt="arrowup"
-                style={{ height: "50px", width: "50px" }}
+                style={{ height: "80px", width: "80px", border: "none" }}
               />
             </button>
           </div>
-
           <p>
             <input
               type="text"
               name="toconverted"
               placeholder="0"
-              value={convertedCurrency}
+              value={currency2}
               onChange={event => {
-                setConvertedCurrency(event.target.value);
+                setCurrency2(event.target.value);
               }}
             />
-            $
+            <select
+              value={devise2}
+              onChange={event => {
+                setDevise2(event.target.value);
+              }}
+            >
+              {deviseItems.map(deviseName => {
+                return <option value={deviseName}>{deviseName}</option>;
+              })}
+            </select>
           </p>
         </form>
       </div>
